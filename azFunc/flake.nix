1eb0b3cdc81b34b1db333b3b8e794fca94e7bba3
boda <bodazhao@gmail.com>
2023-09-18 16:01:59 +0100

    add azure functions flake

---
+++ azFunc/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Azure Functions development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs-18_x
+            azure-functions-core-tools
+          ];
+        };
+      });
+    };
+}
